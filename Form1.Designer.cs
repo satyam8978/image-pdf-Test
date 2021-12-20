50cadfd06e7d9d921086ef0814a3956dc340a300
satyam <satyam0297@gmail.com>
2021-12-20 19:51:18 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace image_pdf_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblfilename = new System.Windows.Forms.Label();
+            this.txtfname = new System.Windows.Forms.TextBox();
+            this.grdFiles = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " Upload File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblfilename
+            // 
+            this.lblfilename.AutoSize = true;
+            this.lblfilename.Location = new System.Drawing.Point(72, 22);
+            this.lblfilename.Name = "lblfilename";
+            this.lblfilename.Size = new System.Drawing.Size(61, 20);
+            this.lblfilename.TabIndex = 3;
+            this.lblfilename.Text = "Caption";
+            // 
+            // txtfname
+            // 
+            this.txtfname.Location = new System.Drawing.Point(166, 22);
+            this.txtfname.Name = "txtfname";
+            this.txtfname.Size = new System.Drawing.Size(160, 27);
+            this.txtfname.TabIndex = 4;
+            // 
+            // grdFiles
+            // 
+            this.grdFiles.AllowUserToAddRows = false;
+            this.grdFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdFiles.Location = new System.Drawing.Point(183, 226);
+            this.grdFiles.Name = "grdFiles";
+            this.grdFiles.RowHeadersWidth = 51;
+            this.grdFiles.RowTemplate.Height = 29;
+            this.grdFiles.Size = new System.Drawing.Size(704, 188);
+            this.grdFiles.TabIndex = 5;
+            this.grdFiles.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdFiles_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 529);
+            this.Controls.Add(this.grdFiles);
+            this.Controls.Add(this.txtfname);
+            this.Controls.Add(this.lblfilename);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblfilename;
+        private System.Windows.Forms.TextBox txtfname;
+        private System.Windows.Forms.DataGridView grdFiles;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image_pdf_Test
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sConn = new SqlConnection(@"Data Source=SATYAM; Initial Catalog=ImgTask; Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+            BindGrid();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            this.openFileDialog1.Filter = "Pdf Files | *.Pdf|jpg  Files |.jpg";
+            var result = openFileDialog1.ShowDialog();
+
+           
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+
+                string[] f = file.Split('\\');
+                // to get the only file name
+                string fn = f[(f.Length) - 1];
+                string dest = @"C:\Users\satya\source\repos\image-pdf-Test\Files\" + fn;
+
+                //to copy the file to the destination folder
+                File.Copy(file, dest, true);
+
+                //to save to the database
+                if (sConn.State == ConnectionState.Open)
+                {
+                    sConn.Close();
+                }
+                sConn.Open();
+                SqlCommand cmd = new SqlCommand("Sp_FileIns", sConn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@filename", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@Caption", SqlDbType.NVarChar);
+                cmd.Parameters["@filename"].Value = fn;
+                cmd.Parameters["@Caption"].Value = txtfname.Text;
+                cmd.ExecuteNonQuery();
+                sConn.Close();
+                BindGrid();
+                txtfname.Text = "";
+                MessageBox.Show("success");
+
+
+            }
+        }
+     
+        
+        public void BindGrid()
+        {
+            DataTable objtable = new DataTable();
+            try
+            {
+                if (sConn.State == ConnectionState.Open)
+                {
+                    sConn.Close();
+                }
+                sConn.Open();
+                SqlCommand cmd = new SqlCommand("Sp_Filesget", sConn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        
+                     
+                        objtable.Load(reader);
+                        grdFiles.DataSource = objtable;
+                        gridedit();
+                        DataGridViewButtonColumn btnUpd = new DataGridViewButtonColumn();
+                        btnUpd.UseColumnTextForButtonValue = true;
+                        btnUpd.Text = "Update";
+                        
+                        grdFiles.Columns.Add(btnUpd);
+                        btnUpd.UseColumnTextForButtonValue = true;
+                        grdFiles.DataSource = objtable;
+                        DataGridViewButtonColumn btnDel = new DataGridViewButtonColumn();
+                        btnDel.Text = "Delete";
+                    
+                        btnDel.UseColumnTextForButtonValue = true;
+                        grdFiles.Columns.Add(btnDel);
+                        btnDel.UseColumnTextForButtonValue = true;
+
+                    }
+                    else
+                    {
+                        objtable = new DataTable();
+                        objtable.Load(reader);
+                        grdFiles.DataSource = objtable;
+                    }
+                    reader.Close();
+                }
+            }
+            catch
+            {
+
+
+            }
+            finally
+            {
+
+                sConn.Close();
+
+            }
+        }
+        private void grdFiles_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            int currentRow = int.Parse(e.RowIndex.ToString());
+            int currentColumnIndex = int.Parse(e.ColumnIndex.ToString());
+            string command = grdFiles.CurrentCell.Value.ToString();
+         
+                int id = Convert.ToInt32(grdFiles.Rows[currentRow].Cells[2].Value.ToString());
+            
+            if (command == "Update")
+            {
+              
+                string name = grdFiles.Rows[currentRow].Cells[4].Value.ToString();
+
+                //to save to the database
+                if (sConn.State == ConnectionState.Open)
+                {
+                    sConn.Close();
+                }   
+                sConn.Open();
+                SqlCommand cmd = new SqlCommand("Sp_FileUpd", sConn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@id", SqlDbType.Int);
+                cmd.Parameters.Add("@Caption", SqlDbType.NVarChar);
+                cmd.Parameters["@id"].Value = id;
+                cmd.Parameters["@Caption"].Value = name;
+                cmd.ExecuteNonQuery();
+                sConn.Close();
+             
+                MessageBox.Show("Update Successfully");
+               BindGrid();
+            }
+            if (command == "Delete")
+            {
+
+            
+                if (sConn.State == ConnectionState.Open)
+                {
+                    sConn.Close();
+                }
+                sConn.Open();
+                SqlCommand cmd = new SqlCommand("Sp_FileDel", sConn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@id", SqlDbType.Int);
+                cmd.Parameters["@id"].Value = id;
+                cmd.ExecuteNonQuery();
+               
+                sConn.Close();
+                MessageBox.Show("Deleted Successfully");
+                BindGrid();
+            }
+            
+            }
+        public void gridedit()
+        {
+            foreach (DataGridViewColumn dc in grdFiles.Columns)
+            {
+                if (dc.Index.Equals(2))
+                {
+                    dc.ReadOnly = false;
+                }
+                else
+                {
+                    dc.ReadOnly = true;
+                }
+            }
+
+        }
+
+    }
+}
